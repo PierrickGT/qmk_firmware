e9c2fd354b479f26e8b78ef255ae1bc7f9dcf53a
Pierrick Turelier <pierrick@turelier.com>
2025-04-17 14:32:28 -0500

    fix(keyboards): update pierrickgt DZ60 layout

--- keyboards/dz60/keymaps/pierrickgt/config.h
+++ keyboards/dz60/keymaps/pierrickgt/config.h
@@ -1,1 +1,7 @@
 #define GRAVE_ESC_GUI_OVERRIDE    # Always send Escape if GUI is pressed
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#define RGBLIGHT_DEFAULT_HUE 0      // 0° hue = red
+#define RGBLIGHT_DEFAULT_SAT 0      // 0% saturation = white
+#define RGBLIGHT_DEFAULT_VAL 255    // Brightness (0–255)
+#define BACKLIGHT_LEVELS 5
--- keyboards/dz60/keymaps/pierrickgt/keymap.c
+++ keyboards/dz60/keymaps/pierrickgt/keymap.c
@@ -1,6 +1,10 @@
 #include QMK_KEYBOARD_H
+#include "quantum.h"
 
 #define ______ KC_TRNS
+#define KC_GESC LT(_FL, KC_ESC)
+#define KC_SLCK KC_SCROLL_LOCK
+#define RESET QK_BOOT
 
 enum dz60_layers {
   _BL, // Base layer
@@ -54,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FL] = LAYOUT_directional(
       KC_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, RESET,
       ______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, ______, ______, ______, ______, ______,
-      ______, BL_TOGG, BL_STEP, BL_DEC, BL_INC, ______, ______, ______, ______, ______, ______, ______,
+      ______, BL_TOGG, BL_STEP, ______, ______, ______, ______, ______, ______, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
   ),
